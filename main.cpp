f3b2ae2888c2e628933b150761f9b87290b3c7d7
CKShetty4 <ckshetty004@gmail.com>
2024-09-15 07:01:15 +0530

    Initialized player controls and game settings

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <time.h>
+#include <conio.h>
 
 using namespace std;
 
@@ -19,11 +21,10 @@ public:
     }
 
     //basic Functionalities
-    void Rest()
+    void Reset()
     {
-        x=originalX;
-        y=originalY;
-        direction=STOP;
+        x = originalX; y = originalY;
+        direction = STOP;
     }
     void changeDirection(eDir d)
     {
@@ -107,6 +108,48 @@ public:
     }
 };
 
+//Using Both above basic functions to manage game.
+class cGameManger
+{
+private:
+    int width, height;
+    int score1, score2;
+    char up1, down1, up2, down2;
+    bool quit;
+    cBall * ball;
+    cPaddle *player1;
+    cPaddle *player2;
+public:
+    cGameManger(int w, int h)
+    {
+        srand(time(NULL));
+        quit = false;
+        up1 = 'w'; up2 = 'i';
+        down1 = 's'; down2 = 'k';
+        score1 = score2 = 0;
+        width = w; height = h;
+        ball = new cBall(w / 2, h / 2);
+        player1 = new cPaddle(1, h / 2 - 3);
+        player2 = new cPaddle(w - 2, h / 2 - 3);
+    }
+
+    //Clear memory to prevent data leaks
+    ~cGameManger()
+    {
+        delete ball, player1, player2;
+    }
+    void ScoreUp(cPaddle * player)
+    {
+        if (player == player1)
+            score1++;
+        else if (player == player2)
+            score2++;
+
+        ball->Reset();
+        player1->Reset();
+        player2->Reset();
+    }
+};
 int main()
 {
     cPaddle p1(0,0);
